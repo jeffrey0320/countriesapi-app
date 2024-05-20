fb26c265d3f0eccec7906bb63031d09a0bd0a38f
jeffrey0320 <jeffreysofly0320@gmail.com>
2024-05-19 20:51:32 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import moonLogo from "./moon-regular.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">
+      <p className="logoP">Where in the world?</p>
+      <div className="colorMode">
+        <button className="modeBtn">
+          <img src={moonLogo} />
+        </button>
+        <p>Dark Mode</p>
+      </div>
     </div>
   );
 }
