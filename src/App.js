76e2cda4c3fdc0cfdfd6365218b32349e85f9322
jeffrey0320 <jeffreysofly0320@gmail.com>
2024-05-20 20:27:07 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import moonLogo from "./moon-regular.svg";
 
 function App() {
   return (
-    <div className="navbar">
-      <p className="logoP">Where in the world?</p>
-      <div className="colorMode">
-        <button className="modeBtn">
-          <img src={moonLogo} />
-        </button>
-        <p>Dark Mode</p>
+    <>
+      <div className="navbar">
+        <p className="logoP">Where in the world?</p>
+        <div className="colorMode">
+          <button className="modeBtn">
+            <img src={moonLogo} />
+          </button>
+          <p>Dark Mode</p>
+        </div>
       </div>
-    </div>
+      <div className="searchFilter">
+        <input
+          className="search"
+          type="text"
+          placeholder="Search for a country..."
+        />
+        <div>
+          <button className="filterBtn">Filter by Region</button>
+        </div>
+      </div>
+    </>
   );
 }
 
