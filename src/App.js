8dca115b4318b4780868c523ebad014c9cad8caa
jeffrey0320 <jeffreysofly0320@gmail.com>
2024-05-22 21:42:51 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./App.css";
 import moonLogo from "./moon-regular.svg";
+import caretDown from "./arrow-down.png";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const Fetch = async () => {
+      try {
+        const response = await fetch("https://restcountries.com/v3.1/all");
+        if (!response.ok) {
+          throw new Error("HTTP error: Status ${response.status}");
+        }
+        let postsData = await response.json();
+        setData(postsData);
+      } catch (err) {
+        setData(null);
+      }
+    };
+
+    Fetch();
+  }, []);
+
+  function searchForCountry(country) {
+    for (const i in data) {
+      if (data[i].name.common.localeCompare(country)) {
+        return data[i];
+      }
+    }
+  }
+
   return (
     <>
       <div className="navbar">
@@ -19,8 +47,18 @@ function App() {
           type="text"
           placeholder="Search for a country..."
         />
-        <div>
-          <button className="filterBtn">Filter by Region</button>
+        <div className="filterDiv">
+          <button className="filterBtn">
+            Filter by Region <img src={caretDown} />
+          </button>
+        </div>
+        <div className="countriesDiv">
+          <div className="card">
+            <h1>Germany</h1>
+            <p>Population: {searchForCountry("Germany").population}</p>
+            <p>Region: {searchForCountry("Germany").region}</p>
+            <p>Capital: </p>
+          </div>
         </div>
       </div>
     </>
