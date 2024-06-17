f5104cfe3d669013855bef87530bf76f25812110
jeffrey0320 <jeffreysofly0320@gmail.com>
2024-06-16 21:37:26 -0400

    WIP

--- src/Components/Country.jsx
+++ src/Components/Country.jsx
@@ -6,37 +6,41 @@ const Country = ({ data }) => {
   const navigate = useNavigate();
   console.log(data.length);
   return (
-    <div className="countriesDiv">
-      <button
-        className="backButton"
-        onClick={() => {
-          navigate(-1);
-        }}
-      >
-        <img src={leftArrow} />
-        Back
-      </button>
-      {data.map((country, index) => {
-        return (
-          <div className="card" key={index}>
-            <div className="imgDiv">
-              <img
-                src={country.flags.png}
-                alt={country.name.common + ` flag`}
-              />
-            </div>
-            <div className="countryInfo">
-              <h2>{country.name.common}</h2>
-              <div>
-                <p>Population: {country.population}</p>
-                <p>Region: {country.region}</p>
-                <p>Capital: {country.capital}</p>
+    <>
+      <div className="backBtnDiv">
+        <button
+          className="backButton"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <img src={leftArrow} />
+          Back
+        </button>
+      </div>
+      <div className="countriesDiv">
+        {data.map((country, index) => {
+          return (
+            <div className="card" key={index}>
+              <div className="imgDiv">
+                <img
+                  src={country.flags.png}
+                  alt={country.name.common + ` flag`}
+                />
+              </div>
+              <div className="countryInfo">
+                <h2>{country.name.common}</h2>
+                <div>
+                  <p>Population: {country.population}</p>
+                  <p>Region: {country.region}</p>
+                  <p>Capital: {country.capital}</p>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
